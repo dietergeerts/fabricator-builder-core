538dc0f804f30e2df0ffd0ded3e58102444d90a0
Dieter Geerts <dieter@dworks.be>
2017-05-25 22:56:41 +0200

    Added webpack-dev-server support, for easy toolkit development

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ module.exports = function fabricatorBuilderWebpackConfigCreator(options) {
     return {
         target: 'node',
         context: path.resolve(__dirname, './src'),
-        entry: {fabricator: './fabricator-builder.default.js'},
+        entry: {'fabricator-builder': './fabricator-builder.default.js'},
         output: {
             filename: '[name].[hash].js',
             path: path.resolve(options.projectPath, './dist'),
             libraryTarget: 'umd'
         },
+        devServer: {port: 3000},
         plugins: [
             new StaticSiteGeneratorPlugin({crawl: true, paths: [''], globals: defaultJsDomView})
         ]
