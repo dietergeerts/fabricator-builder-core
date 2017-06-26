0c18350e17ba51c3638c5a22c0ef21b943fb564d
Dieter Geerts <dieter@dworks.be>
2017-06-26 19:43:39 +0200

    Added eslint with some recommended settings, wip

---
+++ .eslintrc.js
@@ -0,0 +1,68 @@
+module.exports = {
+    'root': true,
+    'env': {
+        'es6': true,
+    },
+    'plugins': [
+        'filenames',
+        'fp',
+        'lodash-fp',
+        'no-use-extend-native',
+        'optimize-regex',
+        'unicorn',
+    ],
+    'extends': [
+        'eslint:recommended',
+        'plugin:fp/recommended',
+        'plugin:lodash-fp/recommended',
+        'plugin:unicorn/recommended',
+    ],
+    'rules': {
+        'no-use-extend-native/no-use-extend-native': 2,
+        'optimize-regex/optimize-regex': 1,
+    },
+    'overrides': [
+        {
+            'files': ['./src/**/*.js'],
+            'rules': {
+                'filenames/no-index': 2,
+            },
+        },
+        {
+            'files': ['./src/**/*.js'],
+            'excludeFiles': './src/**/*.test.js',
+            'rules': {
+                'filenames/match-exported': [2, 'kebab'],
+            },
+        },
+        {
+            'files': ['./src/**/*.test.js'],
+            'env': {
+                'mocha': true,
+            },
+            'plugins': [
+                'chai-expect',
+                'mocha',
+            ],
+            'rules': {
+                'chai-expect/missing-assertion': 2,
+                'chai-expect/no-inner-compare': 2,
+                'chai-expect/terminating-properties': 1,
+                'filenames/match-regex': [2, '^.+\.test\.js$'],
+                'mocha/no-exclusive-tests': 2,
+            },
+        },
+        {
+            'files': ['./src/fabricator/**/*.js'],
+            'env': {
+                'browser': true,
+            },
+        },
+        {
+            'files': ['./src/fabricator-builder/**/*.js', './*.js'],
+            'env': {
+                'node': true,
+            },
+        },
+    ],
+};
