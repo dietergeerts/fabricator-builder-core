a50d498a9f24dd53731b66f6ded5e80679271fea
Dieter Geerts <dieter@dworks.be>
2017-06-26 14:28:14 +0200

    Added unit testing with mocha

---
+++ src/extract-base-url.test.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+const expect = require('chai').expect;
+const extractBaseUrl = require('./extract-base-url');
+
+describe(extractBaseUrl.name, () => {
+
+    const paths = {
+        '': './',
+        'materials': './../',
+        'materials/': './../',
+        'index.html': './',
+        'materials/index.html': './../',
+        '/': './',
+        '/materials': './../',
+        '/materials/': './../',
+        '/index.html': './',
+        '/materials/index.html': './../',
+    };
+
+    it('should return a relative url prefix', () => {
+        expect(Object.keys(paths).map(extractBaseUrl)).to.all.match(/^.\//);
+    });
+
+    it('should correctly go up the levels that the path is deep', () => {
+        Object.keys(paths).forEach(key => {
+            expect(extractBaseUrl(key)).to.equal(paths[key], `'${key}' should extract to '${paths[key]}'`);
+        });
+    });
+});
